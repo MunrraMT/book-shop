6124dbc06745651ba60afc46c01aa74f0a20317e
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-07-26 16:35:19 -0400

    Retirada do lazy

--- src/components/books/index.js
+++ src/components/books/index.js
@@ -1,20 +1,15 @@
-import { lazy, Suspense } from 'react';
-import Loading from '../../utils/loading';
+import ViewerByAuthor from '../viewer-author';
 import StyledBooks from './styles/styled-books';
-
-const ViewerByAuthor = lazy(() => import('../viewer-author'));
 
 const BooksContent = () => (
   <StyledBooks>
     <StyledBooks.Title>Livros por Autor</StyledBooks.Title>
-    <Suspense fallback={<Loading />}>
-      <section>
-        <ViewerByAuthor author='jung' maxBooks='4' />
-        <ViewerByAuthor author='freud' maxBooks='4' />
-        <ViewerByAuthor author='descartes' maxBooks='4' />
-        <ViewerByAuthor author='kant' maxBooks='4' />
-      </section>
-    </Suspense>
+    <section>
+      <ViewerByAuthor author='jung' maxBooks='4' />
+      <ViewerByAuthor author='freud' maxBooks='4' />
+      <ViewerByAuthor author='descartes' maxBooks='4' />
+      <ViewerByAuthor author='kant' maxBooks='4' />
+    </section>
   </StyledBooks>
 );
 
--- src/pages/books-page.js
+++ src/pages/books-page.js
@@ -1,12 +1,9 @@
-import { lazy, Suspense } from 'react';
-import Loading from '../utils/loading';
-
-const BooksContent = lazy(() => import('../components/books'));
+import BooksContent from '../components/books';
 
 const BooksPage = () => (
-  <Suspense fallback={<Loading />}>
+  <>
     <BooksContent />
-  </Suspense>
+  </>
 );
 
 export default BooksPage;
