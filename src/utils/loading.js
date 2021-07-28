f1adb4f9bce8a3ccb912048aaf9c68e3a1c00c1f
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-07-28 15:32:05 -0400

    Validadores dos dados da API do google books

--- src/utils/loading.js
+++
@@ -1,9 +0,0 @@
-import StyledLoading from './styles/styled-loading';
-
-const Loading = () => (
-  <StyledLoading>
-    <img width='50' height='50' src='./Spin-1s-50px.svg' alt='Loading' />
-  </StyledLoading>
-);
-
-export default Loading;
