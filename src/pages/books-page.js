a1b28a315d36fc122fa20ed0c3002931d6b59a5d
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-07-28 15:32:50 -0400

    União do componente BooksContent com BooksPage

--- src/pages/books-page.js
+++ src/pages/books-page.js
@@ -1,13 +1,30 @@
-import BooksContent from '../components/books';
+import { useContext } from 'react';
+import BooksByAuthor from '../components/books-by-author';
+import BooksBySearch from '../components/books-by-search';
 import Header from '../components/header';
+import { DataContext } from '../providers/data-context';
 
-const BooksPage = () => (
-  <>
-    <Header />
-    <main>
-      <BooksContent />
-    </main>
-  </>
-);
+const BooksPage = () => {
+  const { textSearch } = useContext(DataContext);
+  const maxBooks = 4;
+
+  return (
+    <>
+      <Header />
+      <main>
+        {textSearch === '' ? (
+          <>
+            <BooksByAuthor author='jung' maxBooks={maxBooks} />
+            <BooksByAuthor author='freud' maxBooks={maxBooks} />
+            <BooksByAuthor author='descartes' maxBooks={maxBooks} />
+            <BooksByAuthor author='kant' maxBooks={maxBooks} />
+          </>
+        ) : (
+          <BooksBySearch />
+        )}
+      </main>
+    </>
+  );
+};
 
 export default BooksPage;
