788c4ed2ba5a217972a9456165f00461d1a1b8b4
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-07-28 21:32:01 -0400

    movido tag Main para App.js

--- src/pages/books-page.js
+++ src/pages/books-page.js
@@ -1,46 +1,21 @@
 import { useContext } from 'react';
+import { DataContext } from '../providers/data-context';
 import BooksByAuthor from '../components/books-by-author';
 import BooksBySearch from '../components/books-by-search';
-import Header from '../components/header';
-import { DataContext } from '../providers/data-context';
-import randomNumber from '../utils/random-number';
 
 const BooksPage = () => {
   const { textSearch } = useContext(DataContext);
   const maxBooks = 4;
 
-  return (
+  return textSearch === '' ? (
     <>
-      <Header />
-      <main>
-        {textSearch === '' ? (
-          <>
-            <BooksByAuthor
-              key={randomNumber()}
-              author='jung'
-              maxBooks={maxBooks}
-            />
-            <BooksByAuthor
-              key={randomNumber()}
-              author='freud'
-              maxBooks={maxBooks}
-            />
-            <BooksByAuthor
-              key={randomNumber()}
-              author='descartes'
-              maxBooks={maxBooks}
-            />
-            <BooksByAuthor
-              key={randomNumber()}
-              author='kant'
-              maxBooks={maxBooks}
-            />
-          </>
-        ) : (
-          <BooksBySearch />
-        )}
-      </main>
+      <BooksByAuthor author='jung' maxBooks={maxBooks} />
+      <BooksByAuthor author='freud' maxBooks={maxBooks} />
+      <BooksByAuthor author='descartes' maxBooks={maxBooks} />
+      <BooksByAuthor author='kant' maxBooks={maxBooks} />
     </>
+  ) : (
+    <BooksBySearch />
   );
 };
 
